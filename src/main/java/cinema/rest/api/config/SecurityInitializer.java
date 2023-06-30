f1c61cdc374b275d8fd89e2c50105df0a94aa95e
richn <rolik.alexx@gmail.com>
2023-07-01 00:57:55 +0300

    implement spring security configuration

---
+++ src/main/java/cinema/rest/api/config/SecurityInitializer.java
@@ -0,0 +1,6 @@
+package cinema.rest.api.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
