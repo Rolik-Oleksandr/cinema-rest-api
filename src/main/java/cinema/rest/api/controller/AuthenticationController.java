7a28b2a0bd09317aa897081e18e4cb7e8b332ae2
richn <rolik.alexx@gmail.com>
2023-07-12 00:06:20 +0300

    authentication controller

---
+++ src/main/java/cinema/rest/api/controller/AuthenticationController.java
@@ -0,0 +1,29 @@
+package cinema.rest.api.controller;
+
+import cinema.rest.api.model.User;
+import cinema.rest.api.model.dto.request.UserRequestDto;
+import cinema.rest.api.model.dto.response.UserResponseDto;
+import cinema.rest.api.service.AuthenticationService;
+import cinema.rest.api.service.mapper.ResponseDtoMapper;
+import javax.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+    private final AuthenticationService authService;
+    private final ResponseDtoMapper<UserResponseDto, User> userDtoResponseMapper;
+
+    public AuthenticationController(AuthenticationService authService,
+            ResponseDtoMapper<UserResponseDto, User> userDtoResponseMapper) {
+        this.authService = authService;
+        this.userDtoResponseMapper = userDtoResponseMapper;
+    }
+
+    @PostMapping("/register")
+    public UserResponseDto register(@RequestBody @Valid UserRequestDto requestDto) {
+        User user = authService.register(requestDto.getEmail(), requestDto.getPassword());
+        return userDtoResponseMapper.mapToDto(user);
+    }
+}
